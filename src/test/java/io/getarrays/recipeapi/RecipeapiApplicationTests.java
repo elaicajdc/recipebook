484c7294704f3aba2cfc055e8e014148de125d5d
elaicajdc <58583516+elaicajdc@users.noreply.github.com>
2025-01-21 12:28:27 +0800

    initialize

---
+++ src/test/java/io/getarrays/recipeapi/RecipeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.getarrays.recipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
